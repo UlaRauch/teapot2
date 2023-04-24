326853543a381508357e6b0572690ded1920c126
UlaRauch <71543978+UlaRauch@users.noreply.github.com>
2023-04-24 18:54:25 +0200

    created gateway2, forwarding listteas + maketea, no security

---
+++ gateway2/src/test/java/ra/ula/gateway2/Gateway2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ra.ula.gateway2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gateway2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
