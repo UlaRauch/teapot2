90a61eeb25cf45f544962e3c34d704b97e0ae2e9
UlaRauch <71543978+UlaRauch@users.noreply.github.com>
2023-04-24 23:37:34 +0200

    Create milk service

---
+++ milk/src/main/java/ra/ula/milk/MilkApplication.java
@@ -0,0 +1,26 @@
+package ra.ula.milk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class MilkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MilkApplication.class, args);
+    }
+
+}
+
+@RestController
+@RequestMapping("getmilk")
+class MilkServiceController {
+
+    @GetMapping
+    public String getmilk() {
+        return "Here is the milk for your tea.";
+    }
+}
---
+++ milk/src/test/java/ra/ula/milk/MilkApplicationTests.java
@@ -0,0 +1,13 @@
+package ra.ula.milk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
