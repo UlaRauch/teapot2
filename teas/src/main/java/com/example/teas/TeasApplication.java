95de22112c6ff12eb1f618e61047fd3d72359a7b
UlaRauch <71543978+UlaRauch@users.noreply.github.com>
2023-05-10 23:31:32 +0200

    create readme, make mongodb host a env variable, rm eureka client

--- teas/src/main/java/com/example/teas/TeasApplication.java
+++ teas/src/main/java/com/example/teas/TeasApplication.java
@@ -1,15 +1,7 @@
 package com.example.teas;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @SpringBootApplication
 public class TeasApplication {
@@ -20,6 +12,7 @@ public class TeasApplication {
 
 }
 
+/*
 @RestController
 class ServiceInstanceRestController {
 
@@ -32,3 +25,5 @@ class ServiceInstanceRestController {
         return this.discoveryClient.getInstances(applicationName);
     }
 }
+
+ */
