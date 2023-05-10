4adf94e24d3b1be4a14498d06ae9f4bafdda1d43
UlaRauch <71543978+UlaRauch@users.noreply.github.com>
2023-05-11 00:24:54 +0200

    update compose and readme WIP

--- gateway2/src/main/java/ra/ula/gateway2/security/Gateway2SecurityConfiguration.java
+++ gateway2/src/main/java/ra/ula/gateway2/security/Gateway2SecurityConfiguration.java
@@ -1,11 +1,8 @@
-package ra.ula.gateway2.access;
+package ra.ula.gateway2.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 
