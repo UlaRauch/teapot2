af4d7f46a4fe87d421652cca5732307cf38a0cc1
UlaRauch <71543978+UlaRauch@users.noreply.github.com>
2023-05-12 00:46:04 +0200

    rm security

--- teas/src/main/java/com/example/teas/TeasApplication.java
+++ teas/src/main/java/com/example/teas/TeasApplication.java
@@ -11,19 +11,3 @@ public class TeasApplication {
     }
 
 }
-
-/*
-@RestController
-class ServiceInstanceRestController {
-
-    @Autowired
-    private DiscoveryClient discoveryClient;
-
-    @RequestMapping("/service-instances/{applicationName}")
-    public List<ServiceInstance> serviceInstancesByApplicationName(
-            @PathVariable String applicationName) {
-        return this.discoveryClient.getInstances(applicationName);
-    }
-}
-
- */
