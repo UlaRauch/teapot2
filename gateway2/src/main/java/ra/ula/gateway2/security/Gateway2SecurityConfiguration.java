168c33befa3c7fcba1f9ab2e23b8aefb932f6a8c
UlaRauch <71543978+UlaRauch@users.noreply.github.com>
2023-05-13 00:08:46 +0200

    configure gateway2 as oauth2 client

--- gateway2/src/main/java/ra/ula/gateway2/security/Gateway2SecurityConfiguration.java
+++ gateway2/src/main/java/ra/ula/gateway2/security/Gateway2SecurityConfiguration.java
@@ -3,13 +3,16 @@ package ra.ula.gateway2.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
 
 
 @Configuration
 public class Gateway2SecurityConfiguration {
     @Bean
-    public SecurityWebFilterChain springSecurityWebFilterChain(ServerHttpSecurity http) throws Exception {
+    public SecurityWebFilterChain springSecurityWebFilterChain(ServerHttpSecurity http, ServerLogoutSuccessHandler handler) {
         http
                 .authorizeExchange()
                 .pathMatchers("/hellogateway", "/hellotea/noauth")
@@ -19,7 +22,18 @@ public class Gateway2SecurityConfiguration {
                 .anyExchange()
                 .authenticated()
             .and()
-                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+                .oauth2Login()
+            .and()
+                .logout()
+                .logoutSuccessHandler(handler);
+
         return http.build();
+    }
+    @Bean
+    public ServerLogoutSuccessHandler keycloakLogoutSuccessHandler(ReactiveClientRegistrationRepository repository) {
+        OidcClientInitiatedServerLogoutSuccessHandler oidcClientInitiatedServerLogoutSuccessHandler = new OidcClientInitiatedServerLogoutSuccessHandler(repository);
+
+        oidcClientInitiatedServerLogoutSuccessHandler.setPostLogoutRedirectUri("https://orf.at");
+        return oidcClientInitiatedServerLogoutSuccessHandler;
     }
 }
