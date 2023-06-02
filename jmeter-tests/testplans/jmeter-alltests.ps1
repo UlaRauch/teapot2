897c97b9c8de9c683e6650226fbacd632812289f
Ula Rauch <ursula.rauch@stud.fh-campuswien.ac.at>
2023-06-02 02:06:36 +0200

    testplans separat script lauft, response times komisch?

--- jmeter-tests/testplans/jmeter-alltests.ps1
+++ jmeter-tests/testplans/jmeter-alltests.ps1
@@ -1,23 +1,40 @@
-﻿$testplan = "C:\Users\Ula\Documents\FH\WFP_BA\BA2\teapot2-new\jmeter-tests\testplans\teapot2-plan.jmx"
+﻿# testplans for each endpoint
+$gateway0Helloauth = "C:\Users\Ula\Documents\FH\WFP_BA\BA2\teapot2-new\jmeter-tests\testplans\gateway_0_helloauth.jmx"
+$gatewayClHelloauth = "C:\Users\Ula\Documents\FH\WFP_BA\BA2\teapot2-new\jmeter-tests\testplans\gateway_cl_helloauth.jmx"
+$gatewayRsHelloauth = "C:\Users\Ula\Documents\FH\WFP_BA\BA2\teapot2-new\jmeter-tests\testplans\gateway_rs_helloauth.jmx"
 
-$gateway0helloauth = "C:\Users\Ula\Documents\FH\WFP_BA\BA2\teapot2-new\jmeter-tests\testplans\gateway_0_helloauth.jmx"
-$gatewayClhelloauth = "C:\Users\Ula\Documents\FH\WFP_BA\BA2\teapot2-new\jmeter-tests\testplans\gateway_cl_helloauth.jmx"
-$gatewayRshelloauth = "C:\Users\Ula\Documents\FH\WFP_BA\BA2\teapot2-new\jmeter-tests\testplans\gateway_rs_helloauth.jmx"
+$gateway0Hellotea = "C:\Users\Ula\Documents\FH\WFP_BA\BA2\teapot2-new\jmeter-tests\testplans\gateway_0_hellotea.jmx"
+$gatewayClHellotea = "C:\Users\Ula\Documents\FH\WFP_BA\BA2\teapot2-new\jmeter-tests\testplans\gateway_cl_hellotea.jmx"
+$gatewayRsHellotea = "C:\Users\Ula\Documents\FH\WFP_BA\BA2\teapot2-new\jmeter-tests\testplans\gateway_rs_hellotea.jmx"
 
-pfade nochamal neu!
+$teas0 = "C:\Users\Ula\Documents\FH\WFP_BA\BA2\teapot2-new\jmeter-tests\testplans\teas_rs.jmx"
+$teasRs = "C:\Users\Ula\Documents\FH\WFP_BA\BA2\teapot2-new\jmeter-tests\testplans\teas_rs.jmx"
 
-$outputfile = "C:\Users\Ula\Documents\FH\WFP_BA\BA2\teapot2-new\jmeter-tests\results\teapottests-"
-$outputHTML = "C:\Users\Ula\Documents\FH\WFP_BA\BA2\teapot2-new\jmeter-tests\results\teapottests-"
+$testplans = @($gateway0Helloauth, $gatewayClHelloauth, $gatewayRsHelloauth, $gateway0Hellotea, $gatewayClHellotea, $gatewayRsHellotea, $teas0, $teasRs)
+$endpoints = @("gateway0Helloauth", "gatewayClHelloauth", "gatewayRsHelloauth", "gateway0Hellotea", "gatewayClHellotea", "gatewayRsHellotea", "teas0", "teasRs")
 
+# outputfiles
+$outputFileNameStem = "C:\Users\Ula\Documents\FH\WFP_BA\BA2\teapot2-new\jmeter-tests\results\teapottests-"
+$outputHTMLNameStem = "C:\Users\Ula\Documents\FH\WFP_BA\BA2\teapot2-new\jmeter-tests\results\teapottests-"
+
+# jmeter home location
 $jmeterPath = "C:\Program Files\Apache JMeter\apache-jmeter-5.5\bin\jmeter"
 
-for ($count = 1; $count -le 100; $count += 50) {
-Write-Output "Threadcount: " $count " Start"
+for ($count = 100; $count -le 500; $count += 100) {
 
-$arguments = "-Jthreads=" + $count + " -n -t " + $testplan + " -l " + $outputfile + $count + ".csv -e -o " + $outputHTML + $count
+    for ($i = 0; $i -lt 1; $i++) {
+        $outputFileName =  $outputFileNameStem + $endpoints[$i] + "-" + $count + ".csv"
+        $outputHTMLName = $outputHTMLNameStem + $endpoints[$i] + "-" + $count
+        
+        $TOKEN = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJHVGZ5eG9HVTBjbGxobzhnX3U1SGcwajNucUtjUjdwMUgzS2xXNi1BQTRBIn0.eyJleHAiOjE2ODU2NzM5MDUsImlhdCI6MTY4NTYzNzkwNSwianRpIjoiOTUyZmE2ZTctZmIyMi00ODU1LTkyZWEtNmFhN2QyMzg3ZjFkIiwiaXNzIjoiaHR0cDovL2hvc3QuZG9ja2VyLmludGVybmFsOjEwMDAxL3JlYWxtcy90ZWFwb3QiLCJzdWIiOiI5ZGI1NzI3My1mNDVkLTQ0MGYtOTEwZS04ZGM3NjRjM2JjYjAiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJwb3N0bWFuLWNsaWVudCIsInNlc3Npb25fc3RhdGUiOiI4ZjJiNTQ2NS04ZTY0LTRhOTMtYTg2OC02MzdkZmM3YjIxNzEiLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJ1bWFfYXV0aG9yaXphdGlvbiIsImRlZmF1bHQtcm9sZXMtdGVhcG90Il19LCJzY29wZSI6InByb2ZpbGUgZW1haWwiLCJzaWQiOiI4ZjJiNTQ2NS04ZTY0LTRhOTMtYTg2OC02MzdkZmM3YjIxNzEiLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsInByZWZlcnJlZF91c2VybmFtZSI6InVsYSIsImdpdmVuX25hbWUiOiIiLCJmYW1pbHlfbmFtZSI6IiJ9.L7XIdqomdSR6T52Pn6b9Gtq6g1skBSaUKKborzWjqCyYuPshIUqlNXFQo1_6bVlqHTqwQhpbuxlCOfyeHeFoesfvEHCuTcjMrjP19TghBNydELRnqHUKRx2pTj1UTUF7O7BGwM-Xb-zx4aT6cb7Vf3ZAWYbD39OazFNkbsC40kEXVDrcCBJ1db8dJbODO5XWRFRNNdvNHZfgUN4Pd-6XgfORpPpZuwzV-IlM-17NZelXmxdu9LDxR-nECXAEVw9KgnADDlXXHH2Q4hmLGVHQ2GVd5MibjGvAmy9W97tBqw9Ek-lY0_tCwMEk54Kt4QPosaUh4ap7HBasM8EAi4J4wA"
+        $SESSION = "380448f2-ff15-4b4b-8f45-ce95c15390fb"
 
-$proc = Start-Process -PassThru -FilePath $jmeterPath -ArgumentList $arguments
-$proc.WaitForExit()
+        $arguments = "-JTHREADS=" + $count + " -JTOKEN=" + $TOKEN + " -JSESSION=" + $SESSION + " -n -t " + $testplans[$i] + " -l " + $outputFileName + " -e -o " + $outputHTMLName
+        # Write-Output $arguments
 
-Write-Output "Proc " + $count + " Done"
+        $process = Start-Process -PassThru -FilePath $jmeterPath -ArgumentList $arguments
+        $process.WaitForExit()
+
+        @("sep=,") + (Get-Content $outputFileName) | Set-Content $outputFileName
+        }
 }
--- jmeter-tests/testplans/jmeter-incr-threads.ps1
+++ jmeter-tests/testplans/jmeter-incr-threads.ps1
@@ -1,10 +1,10 @@
-﻿$testplan = "C:\Users\Ula\Documents\FH\WFP_BA\BA2\teapot2-new\jmeter-tests\testplans\teapot2-plan.jmx"
+﻿$testplan = "C:\Users\Ula\Documents\FH\WFP_BA\BA2\teapot2-new\jmeter-tests\testplans\alt\teapot2-plan.jmx"
 $outputfile = "C:\Users\Ula\Documents\FH\WFP_BA\BA2\teapot2-new\jmeter-tests\results\teapottests-"
 $outputHTML = "C:\Users\Ula\Documents\FH\WFP_BA\BA2\teapot2-new\jmeter-tests\results\teapottests-"
 
 $jmeterPath = "C:\Program Files\Apache JMeter\apache-jmeter-5.5\bin\jmeter"
 
-for ($count = 1; $count -le 100; $count += 50) {
+for ($count = 1; $count -le 2; $count += 1) {
 Write-Output "Proc " + $count + " Start"
 
 $arguments = "-Jthreads=" + $count + " -n -t " + $testplan + " -l " + $outputfile + $count + ".csv -e -o " + $outputHTML + $count
